100e5939350dc9bea92241aa256fdebf2043f352
Nghia <nghia3977@gmail.com>
2023-04-24 16:04:07 +0700

    update banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,34 +5,32 @@ import styles from "./Header.module.scss";
 
 function Header() {
     return (
-        <div className={styles.header}>
-            <Navbar bg="light" expand="lg">
-                <Container fluid className="d-flex">
-                    <Navbar.Brand href="#" className="col-4">
-                        <i className="fa-solid fa-film"></i> MovieParadise
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav className="my-2 my-lg-0" navbarScroll>
-                            <Nav.Link href="#" className={styles.navLink}>Lịch Chiếu</Nav.Link>
-                            <Nav.Link href="#" className={styles.navLink}>Cụm Rạp</Nav.Link>
-                            <Nav.Link href="#" className={styles.navLink}>Tin Tức</Nav.Link>
-                            <Nav.Link href="#" className={styles.navLink}>Ứng Dụng</Nav.Link>
-                        </Nav>
-                        <div className="ms-auto">
-                            <a href="#" className={`${styles.userLink} ${styles.borderRight} me-3`}>
-                                <i className="fa-solid fa-user me-2" />
-                                <span className="me-3">Đăng nhập</span>
-                            </a>
-                            <a href="#" className={styles.userLink}>
-                                <i className="fa-regular fa-user me-2" />
-                                <span>Đăng ký</span>
-                            </a>
-                        </div>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar bg="light" expand="lg" className={styles.header}>
+            <Container fluid className="d-flex">
+                <Navbar.Brand href="#" className="col-4">
+                    <i className="fa-solid fa-film"></i> MovieParadise
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="my-2 my-lg-0" navbarScroll>
+                        <Nav.Link href="#" className={styles.navLink}>Lịch Chiếu</Nav.Link>
+                        <Nav.Link href="#" className={styles.navLink}>Cụm Rạp</Nav.Link>
+                        <Nav.Link href="#" className={styles.navLink}>Tin Tức</Nav.Link>
+                        <Nav.Link href="#" className={styles.navLink}>Ứng Dụng</Nav.Link>
+                    </Nav>
+                    <div className="ms-auto">
+                        <a href="#" className={`${styles.userLink} ${styles.borderRight} me-3`}>
+                            <i className="fa-solid fa-user me-2" />
+                            <span className="me-3">Đăng nhập</span>
+                        </a>
+                        <a href="#" className={styles.userLink}>
+                            <i className="fa-regular fa-user me-2" />
+                            <span>Đăng ký</span>
+                        </a>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
     return (
-        <div className={styles.container}>
+        <div>
             <Header />
 
             <Outlet />
